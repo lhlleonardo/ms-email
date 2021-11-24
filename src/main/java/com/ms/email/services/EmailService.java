17fabda92bb176ef83a56c1e361ffe59b97c70f3
Leonardo Henrique Lopes <leonardoo.lopes126@gmail.com>
2021-11-24 18:42:23 -0300

    Add email controller

--- src/main/java/com/ms/email/services/EmailService.java
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.ms.email.services;
 
+import com.ms.email.models.EmailModel;
 import com.ms.email.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,4 +15,7 @@ public class EmailService {
 
     @Autowired
     EmailRepository emailRepository;
+
+    public void sendEmail(EmailModel emailModel) {
+    }
 }
