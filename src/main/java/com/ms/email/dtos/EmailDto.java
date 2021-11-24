9f032bb76fff35cb3b1a672a4f33379723384ece
Leonardo Henrique Lopes <leonardoo.lopes126@gmail.com>
2021-11-24 18:34:37 -0300

    Add email structure

---
+++ src/main/java/com/ms/email/dtos/EmailDto.java
@@ -0,0 +1,33 @@
+package com.ms.email.dtos;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+/*
+ *@Author Leonardo Henrique Lopes
+ *@Date 24/11/2021
+ */
+
+@Data
+public class EmailDto {
+
+    @NotBlank
+    private String ownerRef;
+
+    @Email
+    @NotBlank
+    private String emailFrom;
+
+    @Email
+    @NotBlank
+    private String emailTo;
+
+    @NotBlank
+    private String subject;
+
+    @NotBlank
+    private String text;
+}
---
+++ src/main/java/com/ms/email/enums/StatusEmail.java
@@ -0,0 +1,13 @@
+package com.ms.email.enums;
+
+/*
+ *@Author Leonardo Henrique Lopes
+ *@Date 24/11/2021
+ */
+
+public enum StatusEmail {
+
+    SENT,
+    ERROR;
+
+}
