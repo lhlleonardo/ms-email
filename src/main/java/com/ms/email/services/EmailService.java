d8d1518be2927645d8b579d30b0aa9c429c6f9ca
Leonardo Henrique Lopes <leonardoo.lopes126@gmail.com>
2021-11-24 18:37:41 -0300

    add email service

---
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -0,0 +1,17 @@
+package com.ms.email.services;
+
+import com.ms.email.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/*
+ *@Author Leonardo Henrique Lopes
+ *@Date 24/11/2021
+ */
+
+@Service
+public class EmailService {
+
+    @Autowired
+    EmailRepository emailRepository;
+}
